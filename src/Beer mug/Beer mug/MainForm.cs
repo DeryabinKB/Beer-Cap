3b0c54d492ca160783e20445b3da872d07563441
DeryabinKB <102139176+DeryabinKB@users.noreply.github.com>
2022-12-07 13:19:16 +0700

    Start of coding

---
+++ src/Beer mug/Beer mug/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beer_mug
+{
+    public partial class Beer_Mug : Form
+    {
+        public Beer_Mug()
+        {
+            InitializeComponent();
+        }
+        private Color _incorrectColor = Color.LightPink;
+        private void buildButton_Click(object sender, EventArgs e)
+        {
+            ThicknessTextBox.BackColor = _incorrectColor;
+            highTextBox.BackColor = _incorrectColor;
+            highCorrectLable.Text = "Incorrect";
+            thicknessIncorrectLable.Text = "Incorrect";
+        }
+
+        private void MinimumSizeButtom_Click(object sender, EventArgs e)
+        {
+            double thickness = 5;
+            ThicknessTextBox.Text = thickness.ToString();
+        }
+    }
+}
