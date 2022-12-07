a2f0738766e83f2e4401cd6537d6bcbb3b586a69
DeryabinKB <102139176+DeryabinKB@users.noreply.github.com>
2022-12-07 15:29:07 +0700

    Add files via upload

---
+++ src/BeerMug/BeerMug.View/MainForm.cs
@@ -0,0 +1,19 @@
+
+
+namespace BeerMug.View
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private Color _incorrectColor = Color.LightPink;
+
+        private void buildButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
