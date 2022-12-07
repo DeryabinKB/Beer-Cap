f827e36635b4486ceb2bd49ab05f3a73c84d90c0
DeryabinKB <102139176+DeryabinKB@users.noreply.github.com>
2022-12-07 19:10:33 +0700

    Add files via upload

---
+++ src/BeerMug/BeerMug.Model/BeerMugParametrs.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerMug.Model
+{
+    /// <summary>
+    /// Класс параметры пивной кружки.
+    /// </summary>
+    public class MugParameters
+    {
+        /// <summary>
+        /// Хранение типов параметров и их значений.
+        /// </summary>
+        private readonly Dictionary<MugParametersType, BeerMugParametr> _parameters;
+
+        /// <summary>
+        /// Конструктор пивной кружки.
+        /// </summary>
+        public MugParameters()
+        {
+            _parameters = new Dictionary<MugParametersType, BeerMugParametr>()
+            {
+                { MugParametersType.BelowBottomDiametr, new BeerMugParametr(60, 50, 70) },
+                { MugParametersType.HighBottomDiametr, new BeerMugParametr(90, 80, 100) },
+                { MugParametersType.BottomThickness, new BeerMugParametr(13, 10, 16.5) },
+                { MugParametersType.HeightNeckBottom, new BeerMugParametr(130, 100, 165) },
+                { MugParametersType.WallThickness, new BeerMugParametr(6, 5, 7) },
+                { MugParametersType.MugNeckDiametr, new BeerMugParametr(90, 80, 100) },
+            };
+        }
+
+        /// <summary>
+        /// Установка значения параметра.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="value"></param>
+        public void SetParametrValue(MugParametersType type, double value)
+        {
+            if (!_parameters.TryGetValue(type, out var parameter)) return;
+
+            CheckDependencies(type, value);
+            parameter.Value = value;
+        }
+
+        /// <summary>
+        /// Получение значения параметра.
+        /// </summary>
+        /// <param name="type">Тип параметра пивной кружки.</param>
+        /// <returns>Значение параметра.</returns>
+        /// <exception cref="Exception">Если параметр не существует.</exception>
+        public double GetParameterValue(MugParametersType type)
+        {
+            if (_parameters.TryGetValue(type, out var parameter))
+            {
+                return parameter.Value;
+            }
+            throw new Exception("Parameter does not exist");
+        }
+
+        /// <summary>
+        /// Checks dependent parameters.
+        /// </summary>
+        /// <param name="type">Тип параметра пивной кружки.</param>
+        /// <param name="value">Значение параметра.</param>
+        /// <exception cref="Exception">Если значение параметра некорректно.</exception>
+        private void CheckDependencies(MugParametersType type, double value)
+        {
+            switch (type)
+            {
+                case MugParametersType.BottomThickness:
+                    {
+                        _parameters.TryGetValue(MugParametersType.HeightNeckBottom, out var parameter);
+                        double checkValue = value;
+                        double checkParametr = parameter.Value;
+                        if (checkValue * 10 == checkParametr)
+                        {
+                            throw new Exception(
+                                "Height depends on the bottom thickness in the ratio (Bottom thickness * 10)");
+                        }
+                        break;
+                    }
+                case MugParametersType.HighBottomDiametr:
+                    {
+                        _parameters.TryGetValue(MugParametersType.MugNeckDiametr, out var parameter);
+                        double checkValue = value;
+                        double checkParametr = parameter.Value;
+                        if (checkValue == checkParametr)
+                        {
+                            throw new Exception(
+                                "High bottom diametr depends on the Mug neck diametr in the ratio (Bottom diametr * 1)");
+                        }
+                        break;
+                    }
+                case MugParametersType.BelowBottomDiametr:
+                    {
+                        _parameters.TryGetValue(MugParametersType.HighBottomDiametr, out var parameter);
+                        double checkValue = value;
+                        double checkParametr = parameter.Value;
+                        if ((checkParametr - 30) == checkValue)
+                        {
+                            throw new Exception(
+                                "Below bottom diametr depends on the Mug neck diametr in the ratio (Below bottom diametr +30)");
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        return;
+                    }
+            }
+        }
+    }
+}
---
+++ src/BeerMug/BeerMug.View/MainForm.cs
@@ -0,0 +1,186 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BeerMug.Model;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TextBox = System.Windows.Forms.TextBox;
+
+namespace BeerMug.View
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Цвет поля с некорректным значением
+        /// </summary>
+        private Color _incorrectColor = Color.LightPink;
+
+        /// <summary>
+        /// Параметры пивной кружки.
+        /// </summary>
+        private readonly MugParameters Parameters;
+
+        /// <summary>
+        /// Хранит поле с текстом и его ошибки.
+        /// </summary>
+        private readonly Dictionary<TextBox, string> TextBoxAndError;
+
+        // <summary>
+        /// Хранит поле с текстом и соответстующий тип параметра.
+        /// </summary>
+        private readonly Dictionary<TextBox, MugParametersType> TextBoxToParameterType;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Parameters = new MugParameters();
+            TextBoxToParameterType = new Dictionary<TextBox, MugParametersType>
+            {
+                { lowerRadiusOfTheBottomTextBox, MugParametersType.BelowBottomDiametr },
+                { upperRadiusOfTheBottomTextBox, MugParametersType.HighBottomDiametr },
+                { bottomThicknessTextBox, MugParametersType.BottomThickness },
+                { highTextBox, MugParametersType.HeightNeckBottom },
+                { thicknessTextBox, MugParametersType.WallThickness },
+                { outerDiametrTextBox, MugParametersType.MugNeckDiametr }
+            };
+
+            TextBoxAndError = new Dictionary<TextBox, string>
+            {
+                { lowerRadiusOfTheBottomTextBox, "" },
+                { upperRadiusOfTheBottomTextBox, "" },
+                { bottomThicknessTextBox, "" },
+                { highTextBox, "" },
+                { thicknessTextBox, "" },
+                { outerDiametrTextBox, "" }
+            };
+
+        }
+
+        /// <summary>
+        /// Установка стандартных значений при загрузке формы.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SetDefaultValues(90, 6, 130, 13, 90, 60);
+        }
+
+        /// <summary>
+        /// Установка стандартных значений в форму.
+        /// </summary>
+        /// <param name="belowBottomDiametrValue">Значение нижнего радиуса дна</param>
+        /// <param name="highBottomDiametrValue">Значение верхнего радиуса дна</param>
+        /// <param name="bottomThicknessValue">Значение толщины дна</param>
+        /// <param name="heightNeckBottomValue">Значение высоты кружки</param>
+        /// <param name="wallThicknessValue">Значение толщины стенок кружки</param>
+        /// <param name="mugNeckDiametrValue">Значение внешнего диаметра кружки</param>
+        private void SetDefaultValues(double belowBottomDiametrValue, double highBottomDiametrValue,
+          double bottomThicknessValue, double heightNeckBottomValue, double wallThicknessValue, double mugNeckDiametrValue)
+        {
+            Parameters.SetParametrValue(MugParametersType.BelowBottomDiametr, belowBottomDiametrValue);
+            Parameters.SetParametrValue(MugParametersType.HighBottomDiametr, highBottomDiametrValue);
+            Parameters.SetParametrValue(MugParametersType.BottomThickness, bottomThicknessValue);
+            Parameters.SetParametrValue(MugParametersType.HeightNeckBottom, heightNeckBottomValue);
+            Parameters.SetParametrValue(MugParametersType.WallThickness, wallThicknessValue);
+            Parameters.SetParametrValue(MugParametersType.MugNeckDiametr, mugNeckDiametrValue);
+
+            lowerRadiusOfTheBottomTextBox.Text = belowBottomDiametrValue.ToString();
+            upperRadiusOfTheBottomTextBox.Text = highBottomDiametrValue.ToString();
+            bottomThicknessTextBox.Text = bottomThicknessValue.ToString();
+            highTextBox.Text = heightNeckBottomValue.ToString();
+            outerDiametrTextBox.Text = mugNeckDiametrValue.ToString();
+            thicknessTextBox.Text = wallThicknessValue.ToString();
+        }
+
+        /// <summary>
+        /// Установка значений параметров.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SetParametr(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var isType = TextBoxToParameterType.TryGetValue(textBox, out var type);
+            double.TryParse(textBox.Text, out var value);
+            if (!isType) return;
+            try
+            {
+                Parameters.SetParametrValue(type, value);
+                TextBoxAndError[textBox] = "";
+                correctLable.Text = "";
+            }
+            catch (Exception error)
+            {
+                TextBoxAndError[textBox] = error.Message;
+                correctLable.Text = "damn";
+                textBox.BackColor = _incorrectColor;
+
+            }
+        }
+
+        private bool CheckTextBoxes()
+        {
+            var isError = true;
+            foreach (var item in
+                     TextBoxAndError.Where(item => item.Value != ""))
+            {
+                isError = false;
+                correctLable.Text = ($"in the field{item.Key} error: {item.Value}");
+            }
+            return isError;
+        }
+
+        private void MainForm_Load_1(object sender, EventArgs e)
+        {
+            SetDefaultValues(90, 6, 130, 13, 90, 60);
+        }
+
+        private void MinimumSizeButtom_Click_1(object sender, EventArgs e)
+        {
+            SetDefaultValues(80, 5, 100, 10, 80, 50);
+        }
+
+        private void MaximumSizeButton_Click_1(object sender, EventArgs e)
+        {
+            SetDefaultValues(100, 7, 165, 16.5, 100, 70);
+
+        }
+
+        private void AverageSizeButton_Click_1(object sender, EventArgs e)
+        {
+            SetDefaultValues(90, 6, 132.5, 13.25, 90, 60);
+        }
+
+        private void buildButton_Click_1(object sender, EventArgs e)
+        {
+            if (CheckTextBoxes())
+            {
+                // call builder
+            }
+            else
+            {
+                outerDiametrTextBox.BackColor = _incorrectColor;
+                MessageBox.Show(@"Fill all parametrs");
+            }
+        }
+
+        //private void outerDiametrTextBox_TextChanged(object sender, EventArgs e)
+        //{
+        //    MugParametersType type = new MugParametersType();
+        //    type = MugParametersType.MugNeckDiametr;
+        //    double number;
+        //    double.TryParse(string.Join("", outerDiametrTextBox.Text.Where(c => char.IsDigit(c))), out number);
+        //    Parameters.SetParametrValue(type, number);
+        //}
+
+        //private void thicknessTextBox_TextChanged(object sender, EventArgs e)
+        //{
+        //    MugParametersType type = new MugParametersType();
+        //    type = MugParametersType.WallThickness;
+        //    double number;
+        //    double.TryParse(string.Join("", thicknessTextBox.Text.Where(c => char.IsDigit(c))), out number);
+        //    Parameters.SetParametrValue(type, number);
+        //}
+    }
+}
