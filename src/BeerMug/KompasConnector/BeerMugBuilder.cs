6feaa782610c1d77dd150b3903c45b93aa3b0039
Никита Дерябин <deryabin.2001@inbox.ru>
2022-12-08 15:01:36 +0700

    начало builder

--- src/BeerMug/KompasConnector/BeerMugBuilder.cs
+++ src/BeerMug/KompasConnector/BeerMugBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using BeerMug.Model;
+using KompasAPI7;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,13 +13,31 @@ namespace KompasConnector
         /// <summary>
         /// Сапр апи.
         /// </summary>
-        private KompasConnector _apiService = new KompasConnector();
+        private KompasConnector _connector = new KompasConnector();
 
         public void Builder(MugParameters mugParameters)
         {
-            _apiService.StartKompas();
-            _apiService.CreateDocument();
-            _apiService.SetProperties();
+            _connector.StartKompas();
+            _connector.CreateDocument();
+            _connector.SetProperties();
+            //var upperBottom = double.Parse(mugParameters.Parameters[MugParametersType.BelowBottomDiametr]);
+            //var bottomThickness = double.Parse(mugParameters.Parameters[MugParametersType.BottomThickness]);
+            //var high = double.Parse(mugParameters.Parameters[MugParametersType.HeightNeckBottom]);
+            //var wallThickness = double.Parse(mugParameters.Parameters[MugParametersType.WallThickness]);
+            BuildBottom(50, 80, 10);
+        }
+
+        private void BuildBottom(double lowerBottom, double upperBottom, double bottomThickness)
+        {
+            var pointBelow = new Point3D(0, 0, 0);
+            var pointUpper = new Point3D(100, 0, 0);
+            var sketchBelow = _connector.CreateSketch(3);
+            sketchBelow.CreateCircle(pointBelow, lowerBottom);
+            sketchBelow.EndEdit();
+            var sketch = _connector.CreateSketch(3);
+            sketch.CreateCircle(pointUpper, upperBottom);
+            sketch.EndEdit();
+
 
         }
     }
